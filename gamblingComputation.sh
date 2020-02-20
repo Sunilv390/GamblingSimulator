0b9acb82d521e6d5ac0dd9f0d0622dd0b0ddec3c
Sunilv390 <sunilv390@gmail.com>
2020-02-20 10:54:37 +0530

    Added: While loop for checking gambler won or loss 50% of stake will resign

--- gamblingComputation.sh
+++ gamblingComputation.sh
@@ -7,13 +7,21 @@ STAKE=100
 BET=1
 ISWIN=1
 
+#VARIABLE
+cash=$STAKE
+minimum=$(($STAKE-$STAKE/2))
+maximum=$(($STAKE+$STAKE/2))
+
+while (( $cash != $minimum && $cash != $maximum ))
+do
 #RANDOM CHECK FOR WIN OR LOSS
-randomCheck=$((RANDOM%2))
-if [ $randomCheck -eq $ISWIN ]
-then
-	echo win
-	((cash++))
-else
-	echo losse
-	((cash--))
-fi
+	randomCheck=$((RANDOM%2))
+	if [ $randomCheck -eq $ISWIN ]
+	then
+		echo win
+		((cash++))
+	else
+		echo losse
+		((cash--))
+	fi
+done
