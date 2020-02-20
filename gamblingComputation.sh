f8fa5235b939eeced43742edb2101e152478034c
Sunilv390 <sunilv390@gmail.com>
2020-02-20 11:14:30 +0530

    Added:total amount won and loss for 20 days ie.Month

--- gamblingComputation.sh
+++ gamblingComputation.sh
@@ -7,21 +7,26 @@ STAKE=100
 BET=1
 ISWIN=1
 
-#VARIABLE
-cash=$STAKE
-minimum=$(($STAKE-$STAKE/2))
-maximum=$(($STAKE+$STAKE/2))
-
-while (( $cash != $minimum && $cash != $maximum ))
+#FOR LOOP FOR CHECKING FOR A MONTH
+for (( index=1; index<=20; index++ ))
 do
+#VARIABLE
+	cash=$STAKE
+	minimum=$(($STAKE-$STAKE/2))
+	maximum=$(($STAKE+$STAKE/2))
+	while (( $cash != $minimum && $cash != $maximum ))
+	do
 #RANDOM CHECK FOR WIN OR LOSS
-	randomCheck=$((RANDOM%2))
-	if [ $randomCheck -eq $ISWIN ]
-	then
-		echo win
-		((cash++))
-	else
-		echo losse
-		((cash--))
-	fi
+		randomCheck=$((RANDOM%2))
+		if [ $randomCheck -eq $ISWIN ]
+		then
+			echo win
+			((cash++))
+		else
+			echo losse
+			((cash--))
+		fi
+	done
+	totalAmount[index]=$cash
 done
+echo "Total Amount won or loss :: "${totalAmount[@]}
